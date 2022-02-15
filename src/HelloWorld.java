66c81c5139299d938198006a53c541d1de56cf64
JMComstock <jasoncomstock11@gmail.com>
2022-02-15 11:34:30 -0600

    finished hello world exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,23 @@
+// class - defining the class
+// HelloWorld is the name of the class, and it matches the name of the .java file
 public class HelloWorld {
-    public static void main(String[] args) {
+
+    // method - similar to a function in javascript
+    // main method -> specific method in hava that acts as our entry point when running java
+    // (similar to GO starting point in a board game)
+
+    public static void main (String[] args) {
+        //public - pinned for later
+        //static - pinned for later
+        //void - for now, it means this method this doesn't expect anything to be returned
+        //main - name of out method, main is a keyword that is going to be searched as the entry point
+        // String[] args - passing in a string array named args(arguments)
+
+        //print out "Hello World to the console"
         System.out.println("Hello, World!");
 
         System.out.print("Hello, ");
         System.out.print("World!");
+
     }
 }
