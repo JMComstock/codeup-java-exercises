4d70487b70993076b3ae7e48c6fd5ec1e360acfe
JMComstock <jasoncomstock11@gmail.com>
2022-02-22 16:04:08 -0600

    finished Circle exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,30 @@
 package shapes;
 
 public class Circle {
+
+    private static double radius;
+    private static double area;
+    private static double pi = Math.PI;
+    private static double circumference;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public static double getArea() {
+        radius = 3;
+        return area = pi * (radius * radius);
+    }
+
+    public static double getCircumference() {
+        radius = 3;
+        return circumference = 2 * pi * radius;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getArea());
+
+        System.out.println(getCircumference());
+    }
 }
